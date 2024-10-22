bd621a3525d60f6e294756c4a58727dcd154f3f5
Catalina <150844717+Ctlna@users.noreply.github.com.>
2024-10-22 07:41:15 -0500

    style: :lipstick: Mejor

--- js/main.js
+++ js/main.js
@@ -85,21 +85,3 @@ function wheatherDataToday(data){
         `
     }
 }
-
-function fetchWeatherTodaay(){
-    let xhr = new XMLHttpRequest();
-    let apiKey = "5ef27643ffdb4f8cb79164409242110";
-    let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Floridablanca`;
-    xhr.open('GET',url,true);
-    xhr.onreadystatechange=function(){
-        if (this.readyState===4 && this.status===200){
-            let response = JSON.parse(this.responseText);
-            console.log(response);
-            wheatherDataToday(response);
-        }
-        else if (this.readyState==4){
-            console.log('Error: ',this.statusText);
-        }
-    };
-    xhr.send();
-}
