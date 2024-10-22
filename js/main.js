6369015c825d851676cb50ee1f541f890d21dc6f
Catalina <150844717+Ctlna@users.noreply.github.com.>
2024-10-22 16:20:25 -0500

    feat: :construction: Más código

--- js/main.js
+++ js/main.js
@@ -30,6 +30,7 @@ function fetchWeatherTodaay(){
             let response = JSON.parse(this.responseText);
             console.log(response);
             wheatherDataToday(response);
+            fetchWeatherMuchos();
         }
         else if (this.readyState==4){
             console.log('Error: ',this.statusText);
@@ -38,10 +39,30 @@ function fetchWeatherTodaay(){
     xhr.send();
 }
 
+function fetchWeatherMuchos(){
+    let xhr = new XMLHttpRequest();
+    let apiKeys = "5ef27643ffdb4f8cb79164409242110";
+    let url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKeys}&q=Floridablanca&lang=es&days=14`;
+    xhr.open('GET',url,true);
+    xhr.onreadystatechange=function(){
+        if (this.readyState===4 && this.status===200){
+            let response = JSON.parse(this.responseText);
+            console.log(response);
+            wheather10days(response);
+            wheatherHourly(response);
+        }
+        else if (this.readyState==4){
+            console.log('Error: ',this.statusText);
+        }
+    };
+    xhr.send();
+}
+
+
 function wheatherDataToday(data){
     let lugarbusque=document.getElementById('lugarYBusqueda');
     let feels=document.getElementById('FeelsLike');
-    let cositos=document.getElementById('datos')
+    let cositos=document.getElementById('datos');
     if (data.response==="error"){
         lugarbusque.innerHTML=`<p>Error:${data.error}</p>`;
         feels.innerHTML=`<p>Error:${data.error}</p>`;
@@ -83,5 +104,70 @@ function wheatherDataToday(data){
             ${data.current.uv}</p>
         </div>
         `
+    }
+}
+
+function wheatherHourly(data){
+    let contenido=document.getElementById('contenido');
+    let twiligth=document.getElementById('sol');
+    if (data.response==="error"){
+        contenido.innerHTML=`<p>Error:${data.error}</p>`;
+        twiligth.innerHTML=`<p>Error:${data.error}</p>`;
+    }
+    else{
+        let solecito = data.forecast.forecastday[0].astro;
+        let forecast = data.forecast.forecastday[0].hour; 
+        let firstSixHours = forecast.slice(0, 24);
+        let htmlContent = '';
+        firstSixHours.forEach(hour => {
+            htmlContent += `
+                <div class="forecast-hour">
+                    <p class="separa">${hour.time.split(' ')[1]}</p> <!-- Mostrar solo la hora -->
+                    <img class="icon" src="https:${hour.condition.icon}" alt="Weather Icon">
+                    <p class="separa">${hour.temp_c}°</p>
+                </div>
+            `;
+        
+        });
+        contenido.innerHTML=htmlContent;
+        twiligth.innerHTML=`
+            <div class="amanecer">
+                <img class="chiquito" src="./storage/img/nights_stay.png">
+                <p>Sunrise
+                <br>
+                ${solecito.sunrise}</p>
+            </div>
+            <div class="anochecer">
+                <img class="chiquito" src="./storage/img/bounding_box.png">
+                <p>Sunset
+                <br>
+                ${solecito.sunset} </p>
+            </div>
+        `
+    }
+}
+
+function wheather10days(data){
+    let mas=document.getElementById('10dias');
+    if (data.response==="error"){
+        mas.innerHTML=`<p>Error:${data.error}</p>`;
+    }
+    else{
+        let forecast = data.forecast.forecastday;
+        let firstSixDates = forecast.slice(0, 10);
+        let htmlContent = '';
+        firstSixDates.forEach(day => {
+            htmlContent += `
+                <div class="forecast-day">
+                    <p>${day.date}</p>
+                    <img src="${day.day.condition.icon}" alt="Weather Icon">
+                    <p>${day.day.avgtemp_c}°</p>
+                    <p>${day.day.condition.text}</p>
+                </div>
+            `;
+        });
+
+        
+        mas.innerHTML=htmlContent
     }
 }
